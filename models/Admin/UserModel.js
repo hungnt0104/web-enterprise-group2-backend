56e79bd38e9d787c9aab1ce57251c4816f319857
hungnt0104 <111101468+hungnt0104@users.noreply.github.com>
2024-04-14 00:21:12 +0700

    Update UserModel.js

--- models/Admin/UserModel.js
+++ models/Admin/UserModel.js
@@ -4,10 +4,10 @@ var UserSchema = mongoose.Schema(
   name: String,
   email: {type: String, unique: true},
   password: String,
-<<<<<<< HEAD
-  department: String,
-  role: String
-=======
+// <<<<<<< HEAD
+//   department: String,
+//   role: String
+// =======
   role: String,
   department: String
 >>>>>>> 9840d61 (Change multiple files)
